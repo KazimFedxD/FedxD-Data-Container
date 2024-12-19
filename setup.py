5addd35ed00241e316c636c115eaf3d70179c38c
Kazim Abbas <abbaskazim135@gmail.com>
2024-12-19 15:49:48 +0500

    Fixed Python version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(name='fxdc',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     author='FedxD',
     author_email='fedxdofficial@gmail.com',
@@ -17,6 +17,6 @@ setup(name='fxdc',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     
 )
